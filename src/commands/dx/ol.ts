335fea4fd5570ce69ca91db0dfb3082baf50fd78
Qamar Islam <qislam@outlook.com>
2019-10-14 15:47:56 -0400

    enhanced orglist command

--- src/commands/dx/ol.ts
+++ src/commands/dx/ol.ts
@@ -10,10 +10,24 @@ export default class Ol extends Command {
 
   static flags = {
     help: flags.help({char: 'h'}),
-    // flag with a value (-n, --name=VALUE)
+    verbose: flags.boolean({char: 'v'}),
   }
 
   async run() {
-    sfdx.org.list().then((result: any) => this.log(result))
+    const {flags} = this.parse(Ol)
+    if (flags.verbose) {
+      sfdx.org.list().then((result: any) => this.log(result))
+    } else {
+      sfdx.org.list()
+        .then((result: Object) => {
+          for (let orgList of Object.values(result)) {
+            for (let org of orgList) {
+              let orgAlias = org.alias == undefined ? ''.padEnd(10) : org.alias.padEnd(10);
+              this.log(`${orgAlias}\t${org.username.padEnd(35)}\t${org.instanceUrl}`)
+            }
+          }
+        })
+    }
+    
   }
 }
