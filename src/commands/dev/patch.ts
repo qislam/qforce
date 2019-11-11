d959293a106487cc52656550c07cf1824201dc66
Qamar Islam <qislam@outlook.com>
2019-11-10 20:13:11 -0500

    ignoring .qforce

--- src/commands/dev/patch.ts
+++ src/commands/dev/patch.ts
@@ -1,5 +1,7 @@
 import {Command, flags} from '@oclif/command'
 import cli from 'cli-ux'
+import {getRelativePath} from '../../helper/utility'
+const execa = require('execa')
 const path = require('path')
 const fs = require('fs')
 
@@ -25,6 +27,13 @@ export default class DevPatch extends Command {
     }
     const branchA = args.branchA
     const branchB = args.branchB || settings.developBranch
+    const patchPath = settings.patchPath + '/' + branchA.replace(/\//g, '-')
     
+    const mergeBase = await execa('git', ['merge-base', branchA, branchB])
+    const baseCommit = mergeBase.stdout
+    const diff = await execa('git', ['diff', baseCommit, branchA])
+    const diffContent = diff.stdout
+    fs.writeFileSync(getRelativePath(patchPath), diffContent, {encoding: 'utf-8'})
+    cli.action.stop()
   }
 }
