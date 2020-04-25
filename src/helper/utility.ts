b3dce81864586d30c715c2cf6557b613b7d8f3f8
Qamar Islam <qislam@outlook.com>
2020-04-24 21:31:04 -0400

    removed log

--- src/helper/utility.ts
+++ src/helper/utility.ts
@@ -108,7 +108,7 @@ function getQueryAll(query: string, targetusername: string, filter: boolean) {
   return new Promise(
     (resolve, reject) => {
       let sobjecttype = query.substring(query.toLowerCase().lastIndexOf('from'),).split(/\s+/)[1].trim()
-      console.log('sobjecttype: ' + sobjecttype)
+      //console.log('sobjecttype: ' + sobjecttype)
       let defPath = getAbsolutePath('.qforce/definitions/' + sobjecttype + '.json')
       if (fs.existsSync(defPath)) {
         buildQuery(JSON.parse(fs.readFileSync(defPath)))
